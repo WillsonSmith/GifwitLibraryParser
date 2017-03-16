288aa7e1d94267b17ceb728103598b92d3cea38b
Willson Smith <me@willsonsmith.com>
2017-03-15 21:32:57 -0400

    add limit of 20 processes for now

--- lib/gifwit_library_parser.ex
+++ lib/gifwit_library_parser.ex
@@ -1,24 +1,29 @@
 defmodule GifwitLibraryParser do
 
-  def parse do
-    File.read!("giftwitlibrary.gifwit")
+  def parse(file_path) do
+    File.read!(file_path)
     |> Poison.decode!
     |> Map.get("images")
     |> Enum.map(fn(item) -> {convert_keywords_to_names(Map.get(item, "keywords")), Map.get(item, "url")}end)
   end
 
-  def getFile(file) do
-    case HTTPotion.get(elem(file, 1)) do
-      %HTTPotion.Response{ body: body, status_code: 200} ->
-        File.write!("dls/#{elem(file, 0)}", body)
-      _ ->
-        {:err, "not found"}
-    end
+  def getFiles(files) do
+    Enum.each(files, fn(file) ->
+      case HTTPotion.get(elem(file, 1)) do
+        %HTTPotion.Response{ body: body, status_code: 200} ->
+          File.write!("dls/#{elem(file, 0)}", body)
+        _ ->
+          {:err, "not found"}
+      end
+    end)
   end
 
   def download(list) do
-    Enum.each(list, fn(file) -> 
-      spawn(fn -> getFile(file) end)
+    per_spawn = round(Float.ceil(length(list)/20))
+    # create 20 processes to download with
+    Enum.chunk(list, per_spawn, per_spawn, [])
+    |> Enum.each(fn(files) -> 
+      spawn(fn -> getFiles(files) end)
     end)
   end
 
