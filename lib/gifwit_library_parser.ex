0abb21a7978af3dbe2a856bf421159389e6ac7d6
Willson Smith <me@willsonsmith.com>
2017-03-27 21:04:13 -0400

    add download path argument with default as downlaods.nosync

--- lib/gifwit_library_parser.ex
+++ lib/gifwit_library_parser.ex
@@ -4,23 +4,24 @@ defmodule GifwitLibraryParser do
     File.read!(file_path)
     |> Poison.decode!
     |> Map.get("images")
-    |> Enum.map(fn(item) -> {convert_keywords_to_names(Map.get(item, "keywords")), Map.get(item, "url")}end)
+    |> Enum.map(fn(item) -> {convert_keywords_to_names(Map.get(item, "keywords")), Map.get(item, "url")} end)
   end
 
-  def download(list) do
+  def download(list, download_path) do
     list
-    |> Enum.map(&Task.async(fn -> get_file(&1) end))
+    |> Enum.map(&Task.async(fn -> get_file(&1, download_path) end))
     |> Enum.map(&Task.await(&1, 10000))
   end
 
-  def parse_and_download(file_path) do
-    download(parse(file_path))
+  def parse_and_download(file_path, download_path \\ "downloads.nosync") do
+    create_missing_dir(download_path)
+    download(parse(file_path), download_path)
   end
 
-  defp get_file(file) do
+  defp get_file(file, download_path) do
     case HTTPotion.get(elem(file, 1)) do
       %HTTPotion.Response{ body: body, status_code: 200} ->
-        File.write!("downloads.nosync/#{elem(file, 0)}", body)
+        File.write!("#{download_path}/#{elem(file, 0)}", body)
       _ ->
         {:err, "not found"}
     end
@@ -30,5 +31,14 @@ defmodule GifwitLibraryParser do
     transformed = String.split(keywords, " ", trim: true)
     |> Enum.join("-")
     "#{transformed}.gif"
+  end
+
+  defp create_missing_dir(path) do
+    cond do
+      !File.exists?(path) ->
+        File.mkdir(path)
+      true ->
+        nil
+    end
   end
 end
